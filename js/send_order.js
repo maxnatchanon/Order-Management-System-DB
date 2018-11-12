d01cb06033ebcaf482bcc982fd82203ab8b7d2b4
Natchanon A <natchanon.347@gmail.com>
2018-11-12 23:43:29 +0700

    Add file input

--- js/send_order.js
+++ js/send_order.js
@@ -10,21 +10,24 @@ function addItem() {
     modelName.appendChild(input1);
     row.appendChild(modelName);
 
-    var blueprint = document.createElement("div");
-    blueprint.className = "col-3 col";
-    var input2 = document.createElement("input");
-    input2.type = "text";
-    input2.name = "Blueprint";
-    blueprint.appendChild(input2);
-    row.appendChild(blueprint);
-
     var quantity = document.createElement("div");
     quantity.className = "col-1 col";
-    var input3 = document.createElement("input");
-    input3.type = "text";
-    input3.name = "Quantity";
-    quantity.appendChild(input3);
+    var input2 = document.createElement("input");
+    input2.type = "text";
+    input2.name = "Quantity";
+    quantity.appendChild(input2);
     row.appendChild(quantity);
+
+    var blueprint = document.createElement("div");
+    blueprint.className = "col-4 col";
+    var input3 = document.createElement("input");
+    input3.type = "file";
+    input3.name = "Blueprint";
+    input3.accept = "image/*";
+    blueprint.appendChild(input3);
+    row.appendChild(blueprint);
+
+
 
     document.getElementById("items").appendChild(row);
 }
