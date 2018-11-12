69bf8a3ae9c2e66030cb2fccd8a2d8884f803aa9
Natchanon A <natchanon.347@gmail.com>
2018-11-13 00:41:15 +0700

    Add comment

--- js/send_order.js
+++ js/send_order.js
@@ -1,7 +1,10 @@
 function addItem() {
+
+    // Create new row
     var row = document.createElement("div");
     row.className = "row item-data";
 
+    // Create model name text input then append to row
     var modelName = document.createElement("div");
     modelName.className = "col-5 col";
     var input1 = document.createElement("input");
@@ -11,6 +14,7 @@ function addItem() {
     modelName.appendChild(input1);
     row.appendChild(modelName);
 
+    // Create quantity text input then append to row
     var quantity = document.createElement("div");
     quantity.className = "col-1 col";
     var input2 = document.createElement("input");
@@ -20,6 +24,7 @@ function addItem() {
     quantity.appendChild(input2);
     row.appendChild(quantity);
 
+    // Create blueprint picture input then append to row
     var blueprint = document.createElement("div");
     blueprint.className = "col-4 col";
     var input3 = document.createElement("input");
@@ -30,10 +35,14 @@ function addItem() {
     blueprint.appendChild(input3);
     row.appendChild(blueprint);
     
+    // Append row to container
     document.getElementById("items").appendChild(row);
+    
 }
 
 function sendOrder() {
+
+    // Create new http post request
     var xhr = new XMLHttpRequest();
     var url = "/insert";
     xhr.open("POST", url, true);
@@ -43,27 +52,33 @@ function sendOrder() {
             var json = JSON.parse(xhr.responseText);
         }
     };
+    // Create and send data
     var itemList = createObj();
     var data = JSON.stringify(itemList);
     xhr.send(data);
+
 }
 
 function createObj() {
+
+    // Create empty item list with cus_id
     var itemList = {
         cus_id: "", // TODO: Add cus_id
         items: []
     }
+
+    // Get all item-data elements then loop through each element
     var itemData = document.getElementsByClassName("item-data");
     for (var i = 0; i < itemData.length; i++) {
+        // Create item data from each element then append to itemList
         var item = {
-            model_name : "",
-            blueprint : "", 
-            amount : ""
+            model_name : itemData.item(i).getElementsByClassName("modelName")[0].value,
+            blueprint : itemData.item(i).getElementsByClassName("blueprint")[0].value, 
+            amount :itemData.item(i).getElementsByClassName("quantity")[0].value
         }
-        item.model_name = itemData.item(i).getElementsByClassName("modelName")[0].value;
-        item.blueprint = itemData.item(i).getElementsByClassName("blueprint")[0].value;
-        item.amount = itemData.item(i).getElementsByClassName("quantity")[0].value;
         itemList.items.push(item);
     }
+
     return itemList;
+
 }
