eb1087bf82ecd38e52916cd287a35eedf84f71ee
Akamate Chayapiwat <aukmate@gmail.com>
2018-11-14 14:52:14 +0700

    Edit js

--- js/confirm_price.js
+++ js/confirm_price.js
@@ -1,96 +1,90 @@
 const Http = new XMLHttpRequest();
-const url='/selectorder';
+const url = "/selectorder";
 Http.open("GET", url);
-var orderId = {order_id: 1}
+var orderId = { order_id: 1 };
 Http.send(orderId);
 Http.onreadystatechange = function() {
-    if (this.readyState==4 && this.status==200) {
-        var json = JSON.parse(Http.responseText);
+  if (this.readyState == 4 && this.status == 200) {
+    var json = JSON.parse(Http.responseText);
 
-        for (var i = 0; i < json.length; item++) {
-            var row = document.createElement("div");
-            row.className = "row item";
-            row.id = (json[i].model_id).toString();
-            var col1 = document.createElement("div");
-            col1.className = "col-3";
-            col1.innerHTML = json[i].model_name;
-            row.appendChild(col1);
-            var col2 = document.createElement("div");
-            col2.className = "col-2";
-            var btn = document.createElement("button");
-            btn.className = "btn btn-primary";
-            btn.innerHTML = "VIEW";
-            col2.appendChild(btn);
-            row.appendChild(col2);
-            var col3 = document.createElement("div");
-            col3.className = "col-2";
-            var input = document.createElement("input");
-            input.type = "text";
-            col3.appendChild(input);
-            row.appendChild(col3);
-            document.getElementById("orders").appendChild(row);
-        }
+    for (var i = 0; i < json.length; i++) {
+      var row = document.createElement("div");
+      row.className = "row item";
+      row.id = json[i].model_id.toString();
+      var col1 = document.createElement("div");
+      col1.className = "col-3";
+      col1.innerHTML = json[i].model_name;
+      row.appendChild(col1);
+      var col2 = document.createElement("div");
+      col2.className = "col-2";
+      var btn = document.createElement("button");
+      btn.className = "btn btn-primary";
+      btn.innerHTML = "VIEW";
+      col2.appendChild(btn);
+      row.appendChild(col2);
+      var col3 = document.createElement("div");
+      col3.className = "col-2";
+      var input = document.createElement("input");
+      input.type = "text";
+      col3.appendChild(input);
+      row.appendChild(col3);
+      document.getElementById("orders").appendChild(row);
     }
-}
+  }
+};
 
 function confirmPrice() {
-
-    // Lopp through models and send update price request
-    var items = document.getElementsByClassName("item");
-    var sumPrice = 0;
-    for (var i = 0; i < items.length; i++) {
-        var modelId = parseInt(items[i].id);
-        var modelPrice = items[i].getElementsByTagName("input")[0].value;
-        modelPrice = parseInt(modelPrice);
-        sumPrice += modelPrice;
-        sendPriceUpdateReq(modelId, modelPrice)
-    }
-    // Send order update request with sumPrice
-    sendOrderUpdateReq(orderId.order_id, "JuilnwZa", sumPrice);
-    
+  // Lopp through models and send update price request
+  var items = document.getElementsByClassName("item");
+  var sumPrice = 0;
+  for (var i = 0; i < items.length; i++) {
+    var modelId = parseInt(items[i].id);
+    var modelPrice = items[i].getElementsByTagName("input")[0].value;
+    modelPrice = parseInt(modelPrice);
+    sumPrice += modelPrice;
+    sendPriceUpdateReq(modelId, modelPrice);
+  }
+  // Send order update request with sumPrice
+  sendOrderUpdateReq(orderId.order_id, "JuilnwZa", sumPrice);
 }
 
 function sendPriceUpdateReq(modelId, modelPrice) {
-
-    // Create new http post request
-    var xhr = new XMLHttpRequest();
-    var url = "/updateprice";
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var json = JSON.parse(xhr.responseText);
-        }
-    };
-    // Create and send data
-    var obj = {
-        model_id: modelId,
-        model_price: modelPrice
+  // Create new http post request
+  var xhr = new XMLHttpRequest();
+  var url = "/updateprice";
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var json = JSON.parse(xhr.responseText);
     }
-    var data = JSON.stringify(obj);
-    xhr.send(data);
-
+  };
+  // Create and send data
+  var obj = {
+    model_id: modelId,
+    model_price: modelPrice
+  };
+  var data = JSON.stringify(obj);
+  xhr.send(data);
 }
 
 function sendOrderUpdateReq(orderId, adminUsername, quoPrice) {
-
-    // Create new http post request
-    var xhr = new XMLHttpRequest();
-    var url = "/update";
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var json = JSON.parse(xhr.responseText);
-        }
-    };
-    // Create and send data
-    var obj = {
-        order_id : orderId,
-        admin_username : adminUsername,
-        quo_price : quoPrice
+  // Create new http post request
+  var xhr = new XMLHttpRequest();
+  var url = "/update";
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var json = JSON.parse(xhr.responseText);
     }
-    var data = JSON.stringify(obj);
-    xhr.send(data);
-
+  };
+  // Create and send data
+  var obj = {
+    order_id: orderId,
+    admin_username: adminUsername,
+    quo_price: quoPrice
+  };
+  var data = JSON.stringify(obj);
+  xhr.send(data);
 }
--- js/send_order.js
+++ js/send_order.js
@@ -1,93 +1,112 @@
 function addItem() {
+  // Create new row
+  var row = document.createElement("div");
+  row.className = "row item-data";
 
-    // Create new row
-    var row = document.createElement("div");
-    row.className = "row item-data";
+  // Create model name text input then append to row
+  var modelName = document.createElement("div");
+  modelName.className = "col-5 col";
+  var input1 = document.createElement("input");
+  input1.type = "text";
+  input1.name = "ModelName";
+  input1.className = "modelName";
+  modelName.appendChild(input1);
+  row.appendChild(modelName);
 
-    // Create model name text input then append to row
-    var modelName = document.createElement("div");
-    modelName.className = "col-5 col";
-    var input1 = document.createElement("input");
-    input1.type = "text";
-    input1.name = "ModelName";
-    input1.className = "modelName";
-    modelName.appendChild(input1);
-    row.appendChild(modelName);
+  // Create quantity text input then append to row
+  var quantity = document.createElement("div");
+  quantity.className = "col-1 col";
+  var input2 = document.createElement("input");
+  input2.type = "text";
+  input2.name = "Quantity";
+  input2.className = "quantity";
+  quantity.appendChild(input2);
+  row.appendChild(quantity);
 
-    // Create quantity text input then append to row
-    var quantity = document.createElement("div");
-    quantity.className = "col-1 col";
-    var input2 = document.createElement("input");
-    input2.type = "text";
-    input2.name = "Quantity";
-    input2.className = "quantity";
-    quantity.appendChild(input2);
-    row.appendChild(quantity);
+  // Create blueprint picture input then append to row
+  var blueprint = document.createElement("div");
+  blueprint.className = "col-4 col";
+  var input3 = document.createElement("input");
+  input3.type = "file";
+  input3.name = "Blueprint";
+  input3.accept = "image/*";
+  input3.className = "blueprint";
+  blueprint.appendChild(input3);
+  row.appendChild(blueprint);
 
-    // Create blueprint picture input then append to row
-    var blueprint = document.createElement("div");
-    blueprint.className = "col-4 col";
-    var input3 = document.createElement("input");
-    input3.type = "file";
-    input3.name = "Blueprint";
-    input3.accept = "image/*";
-    input3.className = "blueprint";
-    blueprint.appendChild(input3);
-    row.appendChild(blueprint);
-    
-    // Append row to container
-    document.getElementById("items").appendChild(row);
-    
+  // Append row to container
+  document.getElementById("items").appendChild(row);
 }
 
 function sendOrder() {
-
-    // Create new http post request
-    var xhr = new XMLHttpRequest();
-    var url = "/insert";
-    xhr.open("POST", url, true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            var json = JSON.parse(xhr.responseText);
-        }
-    };
-    // Create and send data
-    var itemList = createObj();
-    var data = JSON.stringify(itemList);
-    xhr.send(data);
-
+  // Create new http post request
+  var xhr = new XMLHttpRequest();
+  var url = "/insert";
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var json = JSON.parse(xhr.responseText);
+    }
+  };
+  // Create and send data
+  var itemList = createObj();
+  var data = JSON.stringify(itemList);
+  xhr.send(data);
 }
 
 function createObj() {
+  // Create empty item list with cus_id
+  var itemList = {
+    cus_id: "1", // TODO: Add cus_id
+    items: []
+  };
 
-    // Create empty item list with cus_id
-    var itemList = {
-        cus_id: "1", // TODO: Add cus_id
-        items: []
-    }
+  // Get all item-data elements then loop through each element
+  var itemData = document.getElementsByClassName("item-data");
+  for (var i = 0; i < itemData.length; i++) {
+    // Create item data from each element then append to itemList
+    var item = {
+      model_name: itemData.item(i).getElementsByClassName("modelName")[0].value,
+      blueprint: itemData.item(i).getElementsByClassName("blueprint")[0].value,
+      amount: itemData.item(i).getElementsByClassName("quantity")[0].value
+    };
+    // getImageBinary(
+    //   itemData.item(i).getElementsByClassName("blueprint")[0],
+    //   itemData.item(i).getElementsByClassName("modelName")[0].value
+    // );
+    itemList.items.push(item);
+  }
 
-    // Get all item-data elements then loop through each element
-    var itemData = document.getElementsByClassName("item-data");
-    for (var i = 0; i < itemData.length; i++) {
-        // Create item data from each element then append to itemList
-        var item = {
-            model_name : itemData.item(i).getElementsByClassName("modelName")[0].value,
-            blueprint : getImageBinary(itemData.item(i).getElementsByClassName("blueprint")[0]), 
-            amount :itemData.item(i).getElementsByClassName("quantity")[0].value
-        }
-        itemList.items.push(item);
-    }
-
-    return itemList;
-
+  return itemList;
 }
 
-function getImageBinary(input) {
-    var fReader = new FileReader();
-    fReader.readAsBinaryString(input.files[0]);
-    fReader.onloadend = function(event){
-        return event.target.result;
-        //img.src = 'data:image/jpeg;base64,' + btoa(event.target.result);
+function getImageBinary(input, model_name) {
+  var fReader = new FileReader();
+  fReader.readAsBinaryString(input.files[0]);
+  fReader.onloadend = function(event) {
+    //console.log(event.target.result);
+    if (typeof model_name != "undefined") {
+      sendBlueprintReq(model_name, event.target.result);
+      //console.log({mn: model_name, blueprint: event.target.result});
     }
+    //img.src = 'data:image/jpeg;base64,' + btoa(event.target.result);
+  };
+}
+
+function sendBlueprintReq(modelName, blueprintBin) {
+  // Create new http post request
+  var xhr = new XMLHttpRequest();
+  var url = "/updateblueprint";
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var json = JSON.parse(xhr.responseText);
+    }
+  };
+  // Create and send data
+  var item = { model_name: modelName, blueprint: blueprintBin };
+  var data = JSON.stringify(item);
+  xhr.send(data);
 }
