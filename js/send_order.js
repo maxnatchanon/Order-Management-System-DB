71cd97b08a2d362df74847d50cf87951827aac97
Akamate Chayapiwat <aukmate@gmail.com>
2018-11-15 08:22:06 +0700

    fix

--- js/send_order.js
+++ js/send_order.js
@@ -5,7 +5,7 @@ function addItem() {
 
   // Create model name text input then append to row
   var modelName = document.createElement("div");
-  modelName.className = "col-5";
+  modelName.className = "col-5 col";
   var input1 = document.createElement("input");
   input1.type = "text";
   input1.name = "ModelName";
@@ -15,7 +15,7 @@ function addItem() {
 
   // Create quantity text input then append to row
   var quantity = document.createElement("div");
-  quantity.className = "col-2";
+  quantity.className = "col-1 col";
   var input2 = document.createElement("input");
   input2.type = "text";
   input2.name = "Quantity";
@@ -25,7 +25,7 @@ function addItem() {
 
   // Create blueprint picture input then append to row
   var blueprint = document.createElement("div");
-  blueprint.className = "col-5";
+  blueprint.className = "col-4 col";
   var input3 = document.createElement("input");
   input3.type = "file";
   input3.name = "Blueprint";
@@ -39,7 +39,6 @@ function addItem() {
 }
 
 function sendOrder() {
-  if (!confirm("Send order?")) return;
   // Create new http post request
   var xhr = new XMLHttpRequest();
   var url = "/insert";
@@ -104,7 +103,9 @@ function sendBlueprintReq(modelName, blueprintBin) {
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4 && xhr.status === 200) {
       var json = JSON.parse(xhr.responseText);
+      
     }
+
   };
   // Create and send data
   var item = { model_name: modelName, blueprint: blueprintBin };
